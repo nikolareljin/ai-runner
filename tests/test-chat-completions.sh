82edeb62764e7a55c86bf59a6de61881eb4fcc6d
Nik Reljin <nikola.reljin@gmail.com>
2025-05-19 12:54:49 -0400

    chore: add test scripts for chat endpoints

---
+++ tests/test-chat-completions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./include.sh
+source ./.env
+
+curl http://localhost:11434/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -d '{
+        "model": "deepseek-coder:1.3b",
+        "messages":
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Write a Python script to calculate the factorial of a number."
+            }
+        ]
+    }'
