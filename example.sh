a7a5f1a9cbb92254812bdcc309a4c0a16e397258
Nik Reljin <nikola.reljin@gmail.com>
2026-01-16 08:11:09 -0500

    fix: correct example - include OS library

--- example.sh
+++ example.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SCRIPT_HELPERS_DIR="${SCRIPT_HELPERS_DIR:-$SCRIPT_DIR/scripts/script-helpers}"
 # shellcheck source=/dev/null
 source "$SCRIPT_HELPERS_DIR/helpers.sh"
-shlib_import logging deps help env
+shlib_import logging deps help env os
 
 help() { display_help "$0"; }
 
